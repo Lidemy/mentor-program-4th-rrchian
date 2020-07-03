1b5f619e251e43e1450ecb7fb5e216324c0ef855
rexrexchian <rexrexchian@gmail.com>
2020-07-03 17:44:01 +0800

    already submitted one week later

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,16 +1,10 @@
 function join(arr, concatStr) {
-    var arr_join = [];
-    // 個別加入陣列元素與字串
-    for(i=0; i<=arr.length-1; i++){
-        arr_join.push(arr[i])
-        arr_join.push(concatStr)
-    }
-    // 陣列轉字串
-    var new_str = ""
-    for(i=0; i<=arr_join.length-1; i++){
-        new_str = new_str + arr_join[i]
-    }
-    return new_str
+    var arr_join = '';
+    for (i=0; i<=arr.length-2; i++) {
+        arr_join = arr_join + arr[i] + concatStr;
+        }
+    arr_join += arr[arr.length-1]
+    return arr_join
 }
 
 function repeat(str, times) {
@@ -21,6 +15,29 @@ function repeat(str, times) {
     return str
 }
 
-console.log(join(['a'], '!'));
+console.log(join(["a", "b", "c"], "!"));
 console.log(repeat('a', 5));
 
+/*
+第二，hw5 的 join 那題，請試試看以下幾個 case 是否有回傳正確答案：
+
+``` js
+join([1, 2, 3], '')，正確回傳值：123
+join(["a", "b", "c"], "!")，正確回傳值：a!b!c
+join(["aaa", "bb", "c", "dddd"], ',,')，正確回傳值：aaa,,bb,,c,,dddd
+```
+
+要特別注意的是分隔符號只會在每個元素中間出現，所以如果你寫成：
+
+``` js
+function join(arr, concatStr) {
+  let result = '';
+  for (let i = 0; i < arr.length; i += 1) {
+    result += arr[i] + concatStr;
+  }
+  return result;
+}
+```
+
+是標準錯誤寫法，因為最後面會多了一個 concatStr。
+*/
