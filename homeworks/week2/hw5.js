f3fc7b4ae4ef0c0c36a50e9f39783d1f7e68996e
rexrexchian <rexrexchian@gmail.com>
2020-07-03 17:44:18 +0800

    already submitted one week later

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -4,7 +4,7 @@ function join(arr, concatStr) {
         arr_join = arr_join + arr[i] + concatStr;
         }
     arr_join += arr[arr.length-1]
-    return arr_join
+    return arr_joing
 }
 
 function repeat(str, times) {
@@ -17,27 +17,3 @@ function repeat(str, times) {
 
 console.log(join(["a", "b", "c"], "!"));
 console.log(repeat('a', 5));
-
-/*
-第二，hw5 的 join 那題，請試試看以下幾個 case 是否有回傳正確答案：
-
-``` js
-join([1, 2, 3], '')，正確回傳值：123
-join(["a", "b", "c"], "!")，正確回傳值：a!b!c
-join(["aaa", "bb", "c", "dddd"], ',,')，正確回傳值：aaa,,bb,,c,,dddd
-```
-
-要特別注意的是分隔符號只會在每個元素中間出現，所以如果你寫成：
-
-``` js
-function join(arr, concatStr) {
-  let result = '';
-  for (let i = 0; i < arr.length; i += 1) {
-    result += arr[i] + concatStr;
-  }
-  return result;
-}
-```
-
-是標準錯誤寫法，因為最後面會多了一個 concatStr。
-*/
